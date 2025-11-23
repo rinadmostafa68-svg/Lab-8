0c8ede3277ea3c6e6fa41351596614fc39d0bed8
RinadMostafa <es.renad.mostafa2023@alexu.edu.eg>
2025-11-23 18:47:23 +0200

    v

--- lab8/src/main/java/com/mycompany/lab8/Certificate.java
+++ lab8/src/main/java/com/mycompany/lab8/Certificate.java
@@ -1,3 +1,7 @@
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +12,60 @@
  * @author RinadMostafa
  */
 public class Certificate {
+     private String certificateId;
+    private String studentId;
+    private String courseId;
+    private String issueDate;
+    private String courseTitle;
+
+    public Certificate(String studentId, String courseId, String courseTitle) {
+        this.certificateId = UUID.randomUUID().toString();
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.courseTitle = courseTitle;
+        this.issueDate = LocalDate.now().toString();
+        
+        
+    }
+
+    public String getCertificateId() {
+        return certificateId;
+    }
+
+    public void setCertificateId(String certificateId) {
+        this.certificateId = certificateId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+
+    public void setIssueDate(String issueDate) {
+        this.issueDate = issueDate;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
     
 }
