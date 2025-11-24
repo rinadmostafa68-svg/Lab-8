b80d54db68609c4288375295cb189a65b3bdbf5c
RinadMostafa <es.renad.mostafa2023@alexu.edu.eg>
2025-11-24 02:45:12 +0200

    do

--- lab8/src/main/java/com/mycompany/lab8/Certificate.java
+++ lab8/src/main/java/com/mycompany/lab8/Certificate.java
@@ -1,21 +1,21 @@
-
-import java.time.LocalDate;
-import java.util.UUID;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package backendpkg;
+
+
+import java.util.UUID;
 
 /**
  *
  * @author RinadMostafa
  */
 public class Certificate {
-     private String certificateId;
+
+    private String certificateId;
     private String studentId;
     private String courseId;
-    private String issueDate;
     private String courseTitle;
 
     public Certificate(String studentId, String courseId, String courseTitle) {
@@ -23,7 +23,6 @@ public class Certificate {
         this.studentId = studentId;
         this.courseId = courseId;
         this.courseTitle = courseTitle;
-        this.issueDate = LocalDate.now().toString();
         
         
     }
@@ -52,13 +51,7 @@ public class Certificate {
         this.courseId = courseId;
     }
 
-    public String getIssueDate() {
-        return issueDate;
-    }
 
-    public void setIssueDate(String issueDate) {
-        this.issueDate = issueDate;
-    }
 
     public String getCourseTitle() {
         return courseTitle;
@@ -67,5 +60,5 @@ public class Certificate {
     public void setCourseTitle(String courseTitle) {
         this.courseTitle = courseTitle;
     }
-    
 }
+
