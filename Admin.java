0d54d8aaa49cf534c733ce4f6c820263990840cc
rinadmostafa68-svg <rinadmostafa68@gmail.com>
2025-11-24 02:49:36 +0200

    edit

--- Admin.java
+++ Admin.java
@@ -2,20 +2,41 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Lap_8;
+package backendpkg;
+
+import java.util.List;
+import org.json.JSONObject;
 
 /**
  *
  * @author Zaid&Lama&joody
  */
-public class Admin extends user {
-    public Admin(String username, String password){
-        super(username,password,"ADMIN");
+public class Admin extends User {
+
+    public Admin(String userId, String username, String email, String passwordHash) {
+        super(userId, username, email, passwordHash, Role.ADMIN);
+
     }
-    public void approveCourse(Course course){
+
+    public void approveCourse(Course course) {
         course.setStatus("APPROVED");
     }
-    public void rejectCourse(Course course){
+
+    public void rejectCourse(Course course) {
         course.setStatus("REJECTED");
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject obj = super.toJson();
+        return obj;
+    }
+
+    public static Admin fromJson(JSONObject obj) {
+        String userId = obj.getString("userId");
+        String username = obj.getString("username");
+        String email = obj.getString("email");
+        String passwordHash = obj.getString("passwordHash");
+        return new Admin(userId, username, email, passwordHash);
     }
 }
