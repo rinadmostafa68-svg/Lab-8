3384def946ea3d3d3440b61a057b21ef423d34ed
rinadmostafa68-svg <rinadmostafa68@gmail.com>
2025-11-24 02:51:14 +0200

    edit

---
+++ Certificate.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package backendpkg;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+/**
+ *
+ * @author RinadMostafa
+ */
+public class Certificate {
+
+    private String certificateId;
+    private String studentId;
+    private String courseId;
+    private String courseTitle;
+  List<User> users = new ArrayList<>();
+    public Certificate(String studentId, String courseId, String courseTitle) {
+        this.certificateId = UUID.randomUUID().toString();
+        this.studentId = studentId;
+        this.courseId = courseId;
+        this.courseTitle = courseTitle;
+        
+        
+    }
+
+    public String getCertificateId() {
+        return certificateId;
+    }
+
+    public void setCertificateId(String certificateId) {
+        this.certificateId = certificateId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+}
+
